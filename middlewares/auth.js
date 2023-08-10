491e1dd1a648a3f56c88d7f72a7f5fda30bcae73
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-08-10 21:36:47 +0500

    level-1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,17 +7,22 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization) {
+
+  if (!authorization || !authorization.startsWith) {
     throw new UnauthorizedError(AUTH_REQUIRED);
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_DEV_KEY);
   } catch (err) {
-    next(new UnauthorizedError(AUTH_REQUIRED));
+    throw new UnauthorizedError(AUTH_REQUIRED);
   }
+
   req.user = payload;
+
   next();
 };
 
