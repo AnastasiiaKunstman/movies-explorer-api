6d3156874941fcf1c53b45251751c234b8976b97
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-08-11 10:11:23 +0500

    level-1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,18 @@
+const { NODE_ENV, JWT_SECRET = 'JWT_SECRET' } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/Unauthorized');
-const { AUTH_REQUIRED } = require('../utils/errorMessages');
-
-const { NODE_ENV, JWT_SECRET = 'JWT_SECRET' } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    throw new UnauthorizedError(AUTH_REQUIRED);
+    throw new UnauthorizedError('Неправильные почта или пароль');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new UnauthorizedError(AUTH_REQUIRED));
+    next(new UnauthorizedError('Неправильные почта или пароль'));
   }
   req.user = payload;
   next();
