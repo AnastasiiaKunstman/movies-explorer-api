1b999919e07aa0f55cc9592ae577db49d9f403a0
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-08-11 11:56:48 +0500

    level-1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,23 +3,18 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/Unauthorized');
 
 const auth = (req, res, next) => {
-  let token;
-
-  try {
-    token = req.cookies.jwt;
-  } catch (err) {
-    next(new UnauthorizedError('Неправильные почта или пароль'));
+  const { authorization } = req.headers;
+  if (!authorization) {
+    throw new UnauthorizedError('Неправильные почта или пароль');
   }
-
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Неправильные почта или пароль'));
   }
   req.user = payload;
-
   next();
 };
 
